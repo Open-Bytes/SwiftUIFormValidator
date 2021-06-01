01ba3d9bd72722af5b128f390fd0dcd763274b2a
Shaban <sh3ban.kamel@gmail.com>
2021-06-02 00:07:40 +0200

    Improve EmailValidator

--- Sources/FormValidator/EmailValidator.swift
+++ Sources/FormValidator/EmailValidator.swift
@@ -21,18 +21,13 @@ public class EmailValidator: StringValidator {
     public var value: String = ""
 
     public func validate() -> Validation {
-        guard !value.isEmpty else {
-            return .failure(message: errorMessage())
-        }
-
         let nsPattern = try! NSRegularExpression(
                 pattern: "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$",
                 options: .caseInsensitive)
-        let match = nsPattern.firstMatch(in: value, options: [], range: NSRange(location: 0, length: value.count))
-        if match == nil {
-            return .failure(message: errorMessage())
-        }
-        return .success
+        let patternValidator = PatternValidator(pattern: nsPattern)
+        patternValidator.value = value
+        patternValidator.errorMessage = errorMessage
+        return patternValidator.validate()
     }
 
 }
