d72fa1a71a91d47f27f23b07efd9b0eb0b60faa3
Shaban <sh3ban.kamel@gmail.com>
2021-06-01 23:57:38 +0200

    Improve code

--- Tests/PasswordValidatorTest.swift
+++ Tests/PasswordValidatorTest.swift
@@ -8,6 +8,7 @@ import FormValidator
 
 class PasswordValidatorTest: XCTestCase {
     private var validator: PasswordValidator!
+    let pattern = try! NSRegularExpression(pattern: "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=-_~!*()]).{8,}$")
 
     func testValidator_matching_shouldBeValid() {
         validator = PasswordValidator(firstPassword: "123", secondPassword: "123")
@@ -30,7 +31,6 @@ class PasswordValidatorTest: XCTestCase {
     }
 
     func testValidator_pattern_shouldBeValid() {
-        let pattern = try! NSRegularExpression(pattern: "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=-_~!*()]).{8,}$")
         validator = PasswordValidator(firstPassword: "123SSss@ffkf#", secondPassword: "123SSss@ffkf#", pattern: pattern)
         validator.value = ValidatedPassword(password: "123SSss@ffkf#", type: 0)
         validator.errorMessage = {
@@ -41,7 +41,6 @@ class PasswordValidatorTest: XCTestCase {
     }
 
     func testValidator_pattern_shouldNotBeValid() {
-        let pattern = try! NSRegularExpression(pattern: "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=-_~!*()]).{8,}$")
         validator = PasswordValidator(firstPassword: "123456", secondPassword: "123456", pattern: pattern)
         validator.value = ValidatedPassword(password: "123456", type: 0)
         validator.errorMessage = {
