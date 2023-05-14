2d9b132799e738d173af468eb79d4cddcbb3afcb
Shaban <sh3ban.kamel@gmail.com>
2023-05-14 17:52:03 +0300

    Improve code

--- Sources/FormValidator/FormValidator.swift
+++ Sources/FormValidator/FormValidator.swift
@@ -14,6 +14,7 @@ public protocol Validatable {
     var value: Value? { get set }
 
     func validate() -> Validation
+
     var isEmpty: Bool { get }
 
     var message: StringProducerClosure { get }
